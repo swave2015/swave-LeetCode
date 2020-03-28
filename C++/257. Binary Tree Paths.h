8ac487ece1da91e0b0ad4982853e925df27382e3
swave2015 <caoxiaohang@yeah.net>
2020-03-28 14:06:25 +0800

    257. Binary Tree Paths

---
+++ C++/257. Binary Tree Paths.h
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val): val(val), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        if (root == NULL) {
+            return res;
+        }
+        if (root->left == NULL && root->right == NULL) {
+            res.push_back(to_string(root->val));
+            return res;
+        }
+        vector<string> leftS = binaryTreePaths(root->left);
+        for (int i = 0; i < leftS.size(); i++) {
+            res.push_back(to_string(root->val) + "->" + leftS[i]);
+        }
+        vector<string> rightS = binaryTreePaths(root->right);
+        for (int i = 0; i < rightS.size(); i++) {
+            res.push_back(to_string(root->val) + "->" + rightS[i]);
+        }
+
+        return res;
+    }
+};
