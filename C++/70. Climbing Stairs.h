858411a112fe541ee93be3dac8a2dd776b479179
swave2015 <caoxiaohang@yeah.net>
2020-03-30 17:40:56 +0800

    70. Climbing Stairs

---
+++ C++/70. Climbing Stairs.h
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> ways(n + 1, -1);
+        ways[0] = ways[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+
+        return ways[n];
+    }
+};
