e66235a5519a9cfbd8dcfbc4faef70812151df4c
swave2015 <caoxiaohang@yeah.net>
2020-04-03 18:10:38 +0800

    1143. Longest Common Subsequence

--- C++/1143. Longest Common Subsequence_dp.h
+++ C++/1143. Longest Common Subsequence_dp.h
@@ -9,14 +9,14 @@ public:
         int m = text1.size();
         int n = text2.size();
         vector<vector<int>> memo(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
                 if (text1[i - 1] == text2[j - 1])
                     memo[i][j] = 1 + memo[i - 1][j - 1];
                 else
                     memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]);
-            }
-        }
+
+
 
         return memo[m][n];
     }
